2e3b6851089089766f037734b25594fabe919950
slim8020 <slim8020@naver.com>
2021-07-02 12:54:17 +0900

    Add Input component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import styled, {ThemeProvider} from 'styled-components/native';
 import {theme} from './theme';
+import { StatusBar } from 'react-native';
+import Input from './components/Input';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({theme}) => theme.background};
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({theme})=>theme.main};
+  align-self: flex-start;
+  margin: 20px;
 `;
 
 const App = () => {
   return(
     <ThemeProvider theme={theme}>
-      <Container></Container>
+      <Container>
+        <StatusBar 
+        barStyle="light-content"
+        backgroundColor={theme.background}
+        />
+        <Title>TODO List</Title>
+        <Input/>
+      </Container>
     </ThemeProvider>
   );
 };
---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
+
+const StyledInput = styled.TextInput`
+    width: ${({width})=>width-40}px;
+    height: 60px;
+    margin: 3px 0;
+    padding : 15px 20px;
+    border-radius: 10px;
+    background-color: ${({theme}) => theme.itemBackground};
+    font-size: 25px;
+    color: ${({theme}) => theme.text};
+`;
+
+const Input = () => {
+    const width = Dimensions.get('window').width;
+
+    return <StyledInput width={width} />;
+};
+
+export default Input;
